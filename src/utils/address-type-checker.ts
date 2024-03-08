e4b57cd11ee6a9cc1baf62a2244dc3e73f55fc14
ssoda01 <1443898356@qq.com>
2024-03-08 13:03:46 +0800

    draft

--- src/utils/address-type-checker.ts
+++ src/utils/address-type-checker.ts
@@ -7,12 +7,16 @@ export type AddressType = 'ipV4' | 'ipV6' | 'url' | 'err' | 'empt';
 
 /* Checks if a given string looks like a URL */
 const isUrl = (value: string):boolean => {
-  var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+  var urlPattern = new RegExp(
+    "^(https?:\\/\\/)?" + // validate protocol
+      "((([a-z\\d]+([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+      "(\\:\\d+)?(\\/[\\p{L}\\p{N}\\-._~\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]*)*" + // validate port and path
+      "(\\?[;&a-z\\d%_.~+=\\-]*)?" + // validate query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // validate fragment locator
+
   return urlPattern.test(value);
 };
 
